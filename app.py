6099ba04d3bfdf1e5f7544a5602b503aa9bbc12a
sheetalchouhan475-prog <sheetalchouhan475@gmail.com>
2026-04-03 11:26:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Home route (check server running)
+@app.route('/')
+def home():
+    return "Smart Farming Server Running 🚀"
+
+
+# ESP32 se data receive karne ke liye API
+@app.route('/data', methods=['GET'])
+def get_data():
+    try:
+        # ESP32 se values lena
+        moisture = float(request.args.get('moisture'))
+        temperature = float(request.args.get('temperature'))
+        humidity = float(request.args.get('humidity'))
+
+        print("Moisture:", moisture)
+        print("Temperature:", temperature)
+        print("Humidity:", humidity)
+
+        # Simple logic (irrigation decision)
+        if moisture < 30:
+            irrigation = "ON"
+        else:
+            irrigation = "OFF"
+
+        # Response ESP32 ko bhejna
+        return jsonify({
+            "status": "success",
+            "irrigation": irrigation
+        })
+
+    except:
+        return jsonify({
+            "status": "error",
+            "message": "Invalid data"
+        })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
