882bfe325959ea7a7259f5ca10c40707e0e8d1ca
sheetalchouhan475-prog <sheetalchouhan475@gmail.com>
2026-04-04 12:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,29 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Home route (check server running)
 @app.route('/')
 def home():
     return "Smart Farming Server Running 🚀"
 
-
-# ESP32 se data receive karne ke liye API
 @app.route('/data', methods=['GET'])
 def get_data():
-    try:
-        # ESP32 se values lena
-        moisture = float(request.args.get('moisture'))
-        temperature = float(request.args.get('temperature'))
-        humidity = float(request.args.get('humidity'))
+    moisture = request.args.get('moisture')
+    temperature = request.args.get('temperature')
+    humidity = request.args.get('humidity')
 
-        print("Moisture:", moisture)
-        print("Temperature:", temperature)
-        print("Humidity:", humidity)
+    # 🔥 YEH ADD KARO (IMPORTANT)
+    print("----- NEW DATA RECEIVED -----")
+    print("Moisture:", moisture)
+    print("Temperature:", temperature)
+    print("Humidity:", humidity)
 
-        # Simple logic (irrigation decision)
-        if moisture > 2000 or Temperature > 30 or Humidity < 40:
-    irrigation = "ON"
-     else:
-    irrigation = "OFF"
+    # irrigation logic
+    if float(moisture) < 30:
+        irrigation = "ON"
+    else:
+        irrigation = "OFF"
 
-        # Response ESP32 ko bhejna
-        return jsonify({
-            "status": "success",
-            "irrigation": irrigation
-        })
-
-    except:
-        return jsonify({
-            "status": "error",
-            "message": "Invalid data"
-        })
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return jsonify({
+        "status": "success",
+        "irrigation": irrigation
+    })
